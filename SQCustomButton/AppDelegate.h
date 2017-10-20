ff3175abeb343d60cfd7d5698d38395ddb795bfa
mwi01 <2605072548@qq.com>
2017-10-20 11:10:45 +0800

    First commit

---
+++ SQCustomButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQCustomButton
+//
+//  Created by yangsq on 2017/9/12.
+//  Copyright © 2017年 yangsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
